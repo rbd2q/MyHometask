1307c715085dccfe169f0eeb0fad89971f90d2fa
rbd2q <you@example.comvania-suveev@mail.ru>
2021-05-03 19:25:45 +0300

    My HOmetask

---
+++ main.js
@@ -0,0 +1,10 @@
+let Radius = prompt ('Введите радиус');
+const Pi = 3.14;
+let circleLength = Radius*Pi;
+
+alert (`Длина окружности ${circleLength}`);
+
+const secondsInMinute = 60;
+const MinutesInHour = 60;
+let secondsInHour = secondsInMinute*MinutesInHour;
+console.log  (`Секунд в часу: ${secondsInHour}`)
