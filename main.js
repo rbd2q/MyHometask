288177aca2f8b186e87f3fc7eaa120e83e819aaf
rbd2q <you@example.comvania-suveev@mail.ru>
2021-05-03 21:30:14 +0300

    Add comments

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+/*Задача 1*/
 let Radius = prompt ('Введите радиус');
 const Pi = 3.14;
 let circleLength = Radius*Pi;
-
 alert (`Длина окружности ${circleLength}`);
 
+/*Задача 2*/
 const secondsInMinute = 60;
 const MinutesInHour = 60;
 let secondsInHour = secondsInMinute*MinutesInHour;
