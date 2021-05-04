0fb85192460df64a08a9fec2fb60cef56b85ff26
rbd2q <you@example.comvania-suveev@mail.ru>
2021-05-04 10:01:40 +0300

    Add the third task

--- main.js
+++ main.js
@@ -9,3 +9,25 @@ const secondsInMinute = 60;
 const MinutesInHour = 60;
 let secondsInHour = secondsInMinute*MinutesInHour;
 console.log  (`Секунд в часу: ${secondsInHour}`)
+
+/*Задача 3, пример 1*/
+let num = 47;
+num += 7;
+console.log (`Упрощение num = num + 7: ${num}`);
+num -= 18;
+console.log (`Упрощение num = num - 18;: ${num}`);
+num *= 10;
+console.log (`Упрощение num = num * 10;: ${num}`);
+num /= 15;
+console.log (`Упрощение num = num / 15;: ${num}`);
+alert (`Ответ на первый пример: ${num}`);
+
+/*Задача 3, пример 2*/
+var num2 = 10;
+num2++;
+console.log (`Упрощение num = num2 + 1: ${num2}`);
+num2++;
+console.log (`Упрощение num = num2 + 1: ${num2}`);
+num2--;
+console.log (`Упрощение num = num2 - 1: ${num2}`);
+alert (`Ответ на второй пример: ${num2}`);
